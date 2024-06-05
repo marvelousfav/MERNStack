4c310de0d8b18305b9c5c35b1b0f935200b451b5
marvek16 <marvelousfav1603@gmail.com>
2024-06-05 13:59:26 +0200

    .

--- backend/chapter11/movie_reviews/backend/dao/reviewsDAO.js
+++ backend/chapter11/movie_reviews/backend/dao/reviewsDAO.js
@@ -21,7 +21,7 @@ export default class ReviewsDAO {
                 user_id: user._id,
                 date: date,
                 review: review,
-                movie_id: ObjectId(movieId),
+                movie_id: new ObjectId(movieId),
             }
             return await reviews.insertOne(reviewDoc);
         }
@@ -33,7 +33,7 @@ export default class ReviewsDAO {
     static async updateReview(reviewId, userId, review, date) {
         try {
             const updateResponse = await reviews.updateOne(
-                { user_id: userId, _id: ObjectId(reviewId) },
+                { user_id: userId, _id: new ObjectId(reviewId) },
                 { $set: { review: review, date: date } }
             );
             return updateResponse
@@ -46,7 +46,7 @@ export default class ReviewsDAO {
     static async deleteReview(reviewId, userId) {
         try {
             const deleteResponse = await reviews.deleteOne({
-                _id: ObjectId(reviewId),
+                _id: new ObjectId(reviewId),
                 user_id: userId,
             });
             return deleteResponse
