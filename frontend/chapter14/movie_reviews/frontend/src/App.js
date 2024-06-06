0baef52758d1a37c85bf0f2731b3eaab963c221b
marvek16 <marvelousfav1603@gmail.com>
2024-06-06 12:11:15 +0200

    .

---
+++ frontend/chapter14/movie_reviews/frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Switch, Route, Link } from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css"
+import AddReview from "./components/add-review"
+import MoviesList from "./components/movies-list"
+import Movie from "./components/movie"
+import Login from "./components/login"
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {true ? (
+                <a>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+export default App; 
