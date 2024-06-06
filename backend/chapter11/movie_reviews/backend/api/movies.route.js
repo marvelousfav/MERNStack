e3f29b9b71f0c81a66a0f234dc1e1f01263bddd3
marvek16 <marvelousfav1603@gmail.com>
2024-06-06 10:04:24 +0200

    .

--- backend/chapter11/movie_reviews/backend/api/movies.route.js
+++ backend/chapter11/movie_reviews/backend/api/movies.route.js
@@ -3,6 +3,8 @@ import MoviesController from './movies.controller.js';
 import ReviewsController from './reviews.controller.js';
 const router = express.Router() // get access to express router
 router.route('/').get(MoviesController.apiGetMovies);
+router.route("/id/:id").get(MoviesController.apiGetMovieById) 
+router.route("/ratings").get(MoviesController.apiGetRatings) 
 router
     .route("/review")
     .post(ReviewsController.apiPostReview)
