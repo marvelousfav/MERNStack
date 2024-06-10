d4221a0272b04389647f2c9f1d1595e0e8ee6ece
marvek16 <marvelousfav1603@gmail.com>
2024-06-10 11:44:45 +0200

    .

--- chapter14/movie_reviews/frontend/src/components/login.js
+++ chapter14/movie_reviews/frontend/src/components/login.js
@@ -1,9 +1,62 @@
-import React from 'react'
-function Login() {
+import React, { useState } from 'react'
+import MovieDataService from "../services/movies"
+import { Link } from "react-router-dom"
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+const AddReview = props => {
+    let editing = false
+    let initialReviewState = ""
+    const [review, setReview] = useState(initialReviewState)
+    // keeps track if review is submitted 
+    const [submitted, setSubmitted] = useState(false)
+    const onChangeReview = e => {
+        const review = e.target.value
+        setReview(review);
+    }
+    const saveReview = () => {
+        var data = {
+            review: review,
+            name: props.user.name,
+            user_id: props.user.id,
+            movie_id: props.match.params.id // get movie id direct from url 
+        }
+
+        MovieDataService.createReview(data)
+            .then(response => {
+                setSubmitted(true)
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }
+
     return (
-        <div className="App">
-            Login
+        <div>
+            {submitted ? (
+                <div>
+                    <h4>Review submitted successfully</h4>
+                    <Link to={"/movies/" + props.match.params.id}>
+                        Back to Movie
+                    </Link>
+                </div>
+            ) : (
+                <Form>
+                    <Form.Group>
+                        <Form.Label>{editing ? "Edit" : "Create"} Review</Form.Label>
+                        <Form.Control
+                            type="text"
+                            required
+                            value={review}
+                            onChange={onChangeReview}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" onClick={saveReview}>
+                        Submit
+                    </Button>
+                </Form>
+            )}
         </div>
-    );
+    )
 }
-export default Login; 
+
+export default AddReview; 
