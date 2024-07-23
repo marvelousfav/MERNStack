ec37203cd0e19b99bc181eeb28cc8a8986f779d9
marvek16 <marvelousfav1603@gmail.com>
2024-07-23 14:16:12 +0200

    .

--- Project/blog_app/frontend/next.config.js
+++ Project/blog_app/frontend/next.config.js
@@ -1,8 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
+const withPlugins = require('next-compose-plugins');
+const withCSS = require('@zeit/next-css');
+
+module.exports = withPlugins([
+  [withCSS, {
+    cssModules: true,
+  }],
+], {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: require('styled-jsx/webpack').loader,
+          options: {
+            type: 'scoped'
+          }
+        },
+        'css-loader',
+        'postcss-loader',
+      ],
+    });
+
+    return config;
   }
-  
-  module.exports = nextConfig
-  
+});
+
